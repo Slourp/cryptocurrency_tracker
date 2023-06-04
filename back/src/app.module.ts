9b2dd5090fccba30f40055e3fc8591143d77f424
David VANMAK <david.vanmak.lwg@gmail.com>
2023-06-04 20:28:11 +0200

    [UPDATE] git ignore

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
+import { BookModule } from './book/book.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ScheduleModule } from '@nestjs/schedule';
+import { AuthModule } from './auth/auth.module';
+import { CryptoModule } from './crypto/crypto.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/main'),
+    EventEmitterModule.forRoot(),
+    ScheduleModule.forRoot(),
+    BookModule,
+    AuthModule,
+    CryptoModule,
+  ],
   controllers: [],
   providers: [],
 })
