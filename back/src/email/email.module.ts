53cfb8c5f2349239538b8505be0d077f8d24441d
David VANMAK <david.vanmak.lwg@gmail.com>
2023-06-04 21:14:28 +0200

    [UPDATE] import to use alias and sort

--- back/src/email/email.module.ts
+++ back/src/email/email.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EmailController } from './controller/email/email.controller';
-import { AlertTask } from './tasks/alert.task';
-import { NewsletterTask } from './tasks/newsletter.task';
-import { EmailService } from './services/email/email.service';
-import { EmailQueryService } from './services/email-query/email-query.service';
-import { EmailCommandService } from './services/email-command/email-command.service';
+import { AlertTask } from '@app/email/tasks/alert.task';
+import { NewsletterTask } from '@app/email/tasks/newsletter.task';
+import { EmailService } from '@app/email/services/email/email.service';
+import { EmailQueryService } from '@app/email/services/email-query/email-query.service';
+import { EmailCommandService } from '@app/email/services/email-command/email-command.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EmailSchema } from '@app/user/schemas/user.schema';
 
